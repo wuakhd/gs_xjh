84392297caf31ef57b0c8863d88a96ad9c03fc72
wuakhd <wuhe1209331352@gmail.com>
2025-04-21 15:46:53 +0800

    comit improve

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -438,23 +438,23 @@ class GaussianModel:
                 threshold = torch.quantile(padded_grad, (1.0-ratio))
                 selected_pts_mask = torch.where(padded_grad > threshold, True, False)
 
-        if selected_pts_mask.sum() > 0:
-            stds = self.get_scaling[selected_pts_mask].repeat(N,1)
-            means =torch.zeros((stds.size(0), 3),device="cuda")
-            samples = torch.normal(mean=means, std=stds)
-            rots = build_rotation(self._rotation[selected_pts_mask]).repeat(N,1,1)
-            new_xyz = torch.bmm(rots, samples.unsqueeze(-1)).squeeze(-1) + self.get_xyz[selected_pts_mask].repeat(N, 1)
-            new_scaling = self.scaling_inverse_activation(self.get_scaling[selected_pts_mask].repeat(N,1) / (0.8*N))
-            new_rotation = self._rotation[selected_pts_mask].repeat(N,1)
-            new_features_dc = self._features_dc[selected_pts_mask].repeat(N,1,1)
-            new_features_rest = self._features_rest[selected_pts_mask].repeat(N,1,1)
-            new_opacity = self._opacity[selected_pts_mask].repeat(N,1)
-            new_tmp_radii = self.tmp_radii[selected_pts_mask].repeat(N)
+            if selected_pts_mask.sum() > 0:
+                stds = self.get_scaling[selected_pts_mask].repeat(N,1)
+                means =torch.zeros((stds.size(0), 3),device="cuda")
+                samples = torch.normal(mean=means, std=stds)
+                rots = build_rotation(self._rotation[selected_pts_mask]).repeat(N,1,1)
+                new_xyz = torch.bmm(rots, samples.unsqueeze(-1)).squeeze(-1) + self.get_xyz[selected_pts_mask].repeat(N, 1)
+                new_scaling = self.scaling_inverse_activation(self.get_scaling[selected_pts_mask].repeat(N,1) / (0.8*N))
+                new_rotation = self._rotation[selected_pts_mask].repeat(N,1)
+                new_features_dc = self._features_dc[selected_pts_mask].repeat(N,1,1)
+                new_features_rest = self._features_rest[selected_pts_mask].repeat(N,1,1)
+                new_opacity = self._opacity[selected_pts_mask].repeat(N,1)
+                new_tmp_radii = self.tmp_radii[selected_pts_mask].repeat(N)
 
-            self.densification_postfix(new_xyz, new_features_dc, new_features_rest, new_opacity, new_scaling, new_rotation, new_tmp_radii)
+                self.densification_postfix(new_xyz, new_features_dc, new_features_rest, new_opacity, new_scaling, new_rotation, new_tmp_radii)
 
-            prune_filter = torch.cat((selected_pts_mask, torch.zeros(N * selected_pts_mask.sum(), device="cuda", dtype=bool)))
-            self.prune_points(prune_filter)
+                prune_filter = torch.cat((selected_pts_mask, torch.zeros(N * selected_pts_mask.sum(), device="cuda", dtype=bool)))
+                self.prune_points(prune_filter)
 
     def densify_and_clone(self, grads, grad_threshold, scene_extent, max_gs):
         # Extract points that satisfy the gradient condition
@@ -473,17 +473,17 @@ class GaussianModel:
                 threshold = torch.quantile(grads_tmp, (1.0 - ratio))
                 selected_pts_mask = torch.where(grads_tmp > threshold, True, False)
 
-        if selected_pts_mask.sum() > 0:
-            new_xyz = self._xyz[selected_pts_mask]
-            new_features_dc = self._features_dc[selected_pts_mask]
-            new_features_rest = self._features_rest[selected_pts_mask]
-            new_opacities = self._opacity[selected_pts_mask]
-            new_scaling = self._scaling[selected_pts_mask]
-            new_rotation = self._rotation[selected_pts_mask]
+            if selected_pts_mask.sum() > 0:
+                new_xyz = self._xyz[selected_pts_mask]
+                new_features_dc = self._features_dc[selected_pts_mask]
+                new_features_rest = self._features_rest[selected_pts_mask]
+                new_opacities = self._opacity[selected_pts_mask]
+                new_scaling = self._scaling[selected_pts_mask]
+                new_rotation = self._rotation[selected_pts_mask]
 
-            new_tmp_radii = self.tmp_radii[selected_pts_mask]
+                new_tmp_radii = self.tmp_radii[selected_pts_mask]
 
-            self.densification_postfix(new_xyz, new_features_dc, new_features_rest, new_opacities, new_scaling, new_rotation, new_tmp_radii)
+                self.densification_postfix(new_xyz, new_features_dc, new_features_rest, new_opacities, new_scaling, new_rotation, new_tmp_radii)
 
     def densify_and_prune(self, max_grad, min_opacity, extent, max_screen_size, radii, max_gs):
         grads = self.xyz_gradient_accum / self.denom
